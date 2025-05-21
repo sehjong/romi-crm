caf5483cecb8ea05953c40a420adb36198cdfa90
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2025-05-20 20:43:12 -0500

    refactor routers

--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const clientController = require('../controllers/ClientController');
+const clientController = require("../controllers/clientController");
 
-router.get('/clients', clientController.getClients);
-router.get('/create/client', (req, res) => res.render('clients/create'));
-router.post('/create/client', clientController.createClient);
+router.get("/", clientController.getClients);
+router.get("/new", (req, res) => res.render("clients/new", {title: "New Client"}));
+router.post("/", clientController.createClient);
 
 module.exports = router;
