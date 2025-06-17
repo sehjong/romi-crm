27fee1f549201576d9e2ee7e1d6c466e60969c6e
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2025-06-17 18:04:27 -0500

    reroute dashboard to dashboard controller from posts controller

--- routes/main.js
+++ routes/main.js
@@ -2,13 +2,15 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth");
 const homeController = require("../controllers/home");
-const postsController = require("../controllers/posts");
+// const postsController = require("../controllers/posts");
+const dashboardController = require("../controllers/dashboardController");
 const { ensureAuth } = require("../middleware/auth");
 
 //Main Routes 
 router.get("/", homeController.getIndex);
-router.get("/dashboard", ensureAuth, postsController.getDashboard);
-router.get("/feed", ensureAuth, postsController.getFeed);
+// router.get("/dashboard", ensureAuth, postsController.getDashboard);
+router.get("/dashboard", ensureAuth, dashboardController.getDashboard);
+// router.get("/feed", ensureAuth, postsController.getFeed);
 
 // Route for the Dashboard
 // router.get("/dashboard", ensureAuth, (req, res) => {
