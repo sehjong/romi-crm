ae2fa6d7bceb33bbb028f525821f0d4d376342be
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2025-06-23 14:22:41 -0500

    add routes for client detail, edit, update, and delete

--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -2,8 +2,25 @@ const express = require("express");
 const router = express.Router();
 const clientController = require("../controllers/clientController");
 
+// Shows all clients
 router.get("/", clientController.getClients);
+
+// Shows the form to create a new client
 router.get("/new-client", (req, res) => res.render("clients/newClient", {title: "New Client"}));
+
+// Creates a new client
 router.post("/", clientController.createClient);
+
+// Shows one client
+router.get("/:id", clientController.showClient);
+
+// Edits a client
+router.get("/:id/edit", clientController.editClient);
+
+// Updates a client
+router.put("/:id", clientController.updateClient);
+
+// Deletes a client
+router.delete("/:id", clientController.deleteClient);
 
 module.exports = router;
